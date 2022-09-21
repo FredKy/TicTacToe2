61d747051fcd6eaf40bc0c21f60bb2e33db00f5a
FredKy <90769096+FredKy@users.noreply.github.com>
2022-09-21 10:39:59 +0200

    Initial Commit

---
+++ TicTacToe2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicTacToe2
+//
+//  Created by Fredrik Kypta on 2022-09-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
